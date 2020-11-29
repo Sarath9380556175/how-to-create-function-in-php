fdadcc21171a820f38ce8021f10ddbd3937922ab
Bujala Sarath Kumar Reddy <sarathbunny75@gmail.com>
2020-11-29 10:51:48 +0530

    Function creation and calling(inside function definition)

---
+++ php function creation.php
@@ -0,0 +1,15 @@
+<?php
+function sarath()  //we created a function by declaring functions inside the function 
+{
+	$a=10;  //local variable
+	$b=20;   //local variable
+	$name="Bujala sarah kumar reddy";  //local variable
+	echo "value of a is:" .$a;
+	echo "<br>";
+	echo "value of b is:" .$b;
+	echo "<br>";
+	echo "Myname is:" .$name;
+}
+sarath();
+?>
+

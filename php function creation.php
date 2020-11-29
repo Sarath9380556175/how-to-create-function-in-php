37467d1c3b81c83f3fde7ceaae77081f5299f2d3
Bujala sarath kumar reddy <75170692+Sarath9380556175@users.noreply.github.com>
2020-11-29 11:35:12 +0530

    Update php function creation.php

--- php function creation.php
+++ php function creation.php
@@ -1,3 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Php Function Creation</title>
+    </head>
+
+<style>
+    div{
+        background-color: chartreuse;
+        color:red;
+        width:300px;
+        height:100px;
+        border:10px solid orange;
+        background-image: url("water.jpg");
+    }
+    
+    </style>
+<body>
+    <div>
 <?php
 function sarath()  //we created a function by declaring functions inside the function 
 {
@@ -11,5 +30,20 @@ function sarath()  //we created a function by declaring functions inside the fun
 	echo "Myname is:" .$name;
 }
 sarath();
-?>
+                     //I Created one more function using local variables
+$a=10;
+$name="Bujala sarath kumar reddy";
+function kumar()
+{
+  //note:if we declare varibale and values to the variables outside he function then ihe variable is global variable you can print the value outside the functon only by calling the function()//
+    echo "<br>";
+    
+}
+kumar();
+echo "value of a  is:" .$a ."<br>";
+echo "myname is:" .$name;
 
+?>
+    </div>
+    </body>
+</html>
